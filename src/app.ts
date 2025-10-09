33329d8032ca1069608217b238f217614ab32e79
Esteban vl <estebanvl07@github.com>
2025-10-09 08:23:22 -0500

    FEAT: changes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import express, { NextFunction } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import routes from "@/routes";
 import helmet from "helmet";
 import cors from "cors";
-import { RequestError } from "./utils/errorHandler";
 
 dotenv.config();
 
@@ -14,7 +13,6 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
@@ -23,21 +21,5 @@ app.use(express.urlencoded({ extended: true }));
 
 // ALL ROUTES
 app.use("/api", routes);
-
-// ERROR HANDLER
-app.use((err: RequestError, req: any, res: any, next: NextFunction) => {
-  if (err instanceof RequestError) {
-    return res.status(err.HttpStatusCode).json({
-      message: err.message,
-      code: err.HttpStatusCode,
-    });
-  }
-
-  console.error("❌ Unhandled error:", err);
-
-  return res.status(500).json({
-    message: "Internal Server Error",
-  });
-});
 
 export { app };
